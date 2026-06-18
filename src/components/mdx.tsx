5ae23e61f106cb5a1bcacf260d27be25899905cb
Danielle <debbiemeer30@gmail.com>
2026-06-19 01:36:14 +0800

    updated mdx components with protocol facts

--- src/components/mdx.tsx
+++ src/components/mdx.tsx
@@ -1,9 +1,11 @@
 import defaultMdxComponents from 'fumadocs-ui/mdx';
 import type { MDXComponents } from 'mdx/types';
+import * as ProtocolFacts from '@/components/protocol-facts';
 
 export function getMDXComponents(components?: MDXComponents) {
   return {
     ...defaultMdxComponents,
+    ...ProtocolFacts,
     ...components,
   } satisfies MDXComponents;
 }
