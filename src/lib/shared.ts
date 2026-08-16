7ca2fd45c98f705579cfbc2052966af946c2df7e
Danielle <debbiemeer30@gmail.com>
2026-08-16 11:19:47 +0800

    updated shared.ts to drop unused appName and docs og/content routes

--- src/lib/shared.ts
+++ src/lib/shared.ts
@@ -1,8 +1,4 @@
-export const appName = 'Zentra';
-
 export const docsRoute = '/docs';
-export const docsImageRoute = '/og/docs';
-export const docsContentRoute = '/llms.mdx/docs';
 
 export const gitConfig = {
   user: 'ALGOREX-PH',
